ff566b558dfcb0d21cb04b1be2471896a470d6bc
Ezronki <91666513+Ezronki@users.noreply.github.com>
2025-02-06 15:44:13 +0300

    Update tawk.jsx

--- src/components/tawk/tawk.jsx
+++ src/components/tawk/tawk.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-function TawkToPageTracker() {
+function TawkTo() {
   const location = useLocation();
 
   useEffect(() => {
@@ -27,4 +27,4 @@ function TawkToPageTracker() {
   return null;
 }
 
-export default TawkToPageTracker;
+export default TawkTo;
