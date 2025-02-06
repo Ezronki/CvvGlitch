032f9f3e02dc21fa30bd52e216528034467293aa
Ezronki <91666513+Ezronki@users.noreply.github.com>
2025-02-06 17:01:02 +0300

    Update search.jsx

--- src/pages/shopping-view/search.jsx
+++ src/pages/shopping-view/search.jsx
@@ -1,7 +1,6 @@
 import ProductDetailsDialog from "@/components/shopping-view/product-details";
 import ShoppingProductTile from "@/components/shopping-view/product-tile";
 import { Input } from "@/components/ui/input";
-import { useToast } from "@/components/ui/use-toast";
 
 import { fetchProductDetails } from "@/store/shop/products-slice";
 import {
