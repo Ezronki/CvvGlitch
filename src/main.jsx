ac6af88c031079f0caa71a3ddbf25d87887ca507
Ezronki <91666513+Ezronki@users.noreply.github.com>
2025-02-06 11:41:50 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { Toaster } from "./components/ui/toaster.jsx";
+import TawkToPageTracker from "./components/tawk/TawkToPageTracker";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <TawkToPageTracker />
     <Provider store={store}>
       <App />
       <Toaster />
