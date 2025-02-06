30f710e1c62adf761aa5cf93c39440338e82b622
Ezronki <91666513+Ezronki@users.noreply.github.com>
2025-02-06 05:20:09 +0300

    Update image-upload.jsx

--- src/components/admin-view/image-upload.jsx
+++ src/components/admin-view/image-upload.jsx
@@ -50,7 +50,7 @@ function ProductImageUpload({
     const data = new FormData();
     data.append("my_file", imageFile);
     const response = await axios.post(
-      "http://localhost:3000/api/admin/products/upload-image",
+      "https://backend-o0sl.onrender.com/api/admin/products/upload-image",
       data
     );
     console.log(response, "response");
