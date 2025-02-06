0683a3d98653a8b11be64a6a29a36f925eba7275
Ezronki <91666513+Ezronki@users.noreply.github.com>
2025-02-06 18:53:19 +0300

    Update product-details.jsx

--- src/components/shopping-view/product-details.jsx
+++ src/components/shopping-view/product-details.jsx
@@ -81,7 +81,7 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
               className="aspect-square w-full object-cover"
             />
             <div>
-              <h3 className="text-lg sm:text-xl font-semibold mb-2">Description:</h3>
+              <h3 className="mt-2 text-lg text-center sm:text-xl font-semibold mb-2">Description:</h3>
               <p className="text-muted-foreground text-lg sm:text-xl mt-3 mb-4">
                 {productDetails?.description}
               </p>
