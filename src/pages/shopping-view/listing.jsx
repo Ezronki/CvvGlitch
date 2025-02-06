f164faecb5b115337e6302cc5494d42846edfdc4
Ezronki <91666513+Ezronki@users.noreply.github.com>
2025-02-06 19:38:09 +0300

    Update listing.jsx

--- src/pages/shopping-view/listing.jsx
+++ src/pages/shopping-view/listing.jsx
@@ -39,6 +39,7 @@ const categories = [
   { id: "venmo", label: "Venmo", path: "/shop/venmo" },
   { id: "applepay", label: "ApplePay", path: "/shop/apple-pay" },
   { id: "gpay", label: "GooglePay", path: "/shop/google-pay" },
+  { id: "dump", label: "Dumps", path: "/shop/dump" }
 ];
 
 function ShoppingListing() {
@@ -114,7 +115,7 @@ function ShoppingListing() {
             </DropdownMenu>
           </div>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4">
           {productList && productList.length > 0
             ? productList.map((productItem) => (
               <ShoppingProductTile
