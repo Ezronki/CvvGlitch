a26eb12766265fc7a4198da37b33f657679ffa0a
Ezronki <91666513+Ezronki@users.noreply.github.com>
2025-02-06 16:28:54 +0300

    Update search.jsx

--- src/pages/shopping-view/search.jsx
+++ src/pages/shopping-view/search.jsx
@@ -22,7 +22,7 @@ function SearchProducts() {
 
   const { user } = useSelector((state) => state.auth);
 
-  const { cartItems } = useSelector((state) => state.shopCart);
+ 
   const { toast } = useToast();
   useEffect(() => {
     if (keyword && keyword.trim() !== "" && keyword.trim().length > 3) {
@@ -36,42 +36,7 @@ function SearchProducts() {
     }
   }, [keyword]);
 
-  function handleAddtoCart(getCurrentProductId, getTotalStock) {
-    console.log(cartItems);
-    let getCartItems = cartItems.items || [];
 
-    if (getCartItems.length) {
-      const indexOfCurrentItem = getCartItems.findIndex(
-        (item) => item.productId === getCurrentProductId
-      );
-      if (indexOfCurrentItem > -1) {
-        const getQuantity = getCartItems[indexOfCurrentItem].quantity;
-        if (getQuantity + 1 > getTotalStock) {
-          toast({
-            title: `Only ${getQuantity} quantity can be added for this item`,
-            variant: "destructive",
-          });
-
-          return;
-        }
-      }
-    }
-
-    dispatch(
-      addToCart({
-        userId: user?.id,
-        productId: getCurrentProductId,
-        quantity: 1,
-      })
-    ).then((data) => {
-      if (data?.payload?.success) {
-        dispatch(fetchCartItems(user?.id));
-        toast({
-          title: "Product is added to cart",
-        });
-      }
-    });
-  }
 
   function handleGetProductDetails(getCurrentProductId) {
     console.log(getCurrentProductId);
@@ -103,7 +68,7 @@ function SearchProducts() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {searchResults.map((item) => (
           <ShoppingProductTile
-            handleAddtoCart={handleAddtoCart}
+            
             product={item}
             handleGetProductDetails={handleGetProductDetails}
           />
