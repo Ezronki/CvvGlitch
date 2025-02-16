8ca72c5eb7a45e54cb4e9bbe75e72b0024350b7d
Ezron <ezronkiprotich@gmail.com>
2025-02-16 16:56:02 +0300

    hover

--- src/components/shopping-view/header.jsx
+++ src/components/shopping-view/header.jsx
@@ -143,7 +143,7 @@ function HeaderButtons({ onItemClick, isMobile }) {
   return (
     <div className="flex text-black items-center gap-2">
       <Button
-        className="bg-[#04D94F] font-extrabold text-black rounded-md "
+        className="bg-[#04D94F] font-extrabold hover:bg-white text-black rounded-md "
         onClick={() => {
           navigate('/shop/cart');
         onItemClick();
@@ -155,7 +155,7 @@ function HeaderButtons({ onItemClick, isMobile }) {
         $0.00
       </label>
       <Button
-        className="bg-yellow-500  text-black font-extrabold px-4 py-2 rounded-md "
+        className="bg-yellow-500  text-black hover:bg-white font-extrabold px-4 py-2 rounded-md transition-all"
         onClick={() => {
           navigate('/shop/Orders');
         onItemClick();
