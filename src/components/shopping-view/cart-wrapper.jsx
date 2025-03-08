4a5f2ad576c44d1ac7082fbd24a385c2598148c2
Ezron <ezronkiprotich@gmail.com>
2025-03-08 18:37:06 +0300

    4444

--- src/components/shopping-view/cart-wrapper.jsx
+++ src/components/shopping-view/cart-wrapper.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import { Button } from "../ui/button";
 import { SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
 import UserCartItemsContent from "./cart-items-content";
 
@@ -9,15 +8,20 @@ function UserCartWrapper({ cartItems, setOpenCartSheet }) {
   const totalCartAmount =
     cartItems && cartItems.length > 0
       ? cartItems.reduce(
-        (sum, currentItem) =>
-          sum +
-          (currentItem?.salePrice > 0
-            ? currentItem?.salePrice
-            : currentItem?.price) *
-          currentItem?.quantity,
-        0
-      )
+          (sum, currentItem) =>
+            sum +
+            (currentItem?.salePrice > 0
+              ? currentItem?.salePrice
+              : currentItem?.price) *
+              currentItem?.quantity,
+          0
+        )
       : 0;
+
+  const handleCheckout = () => {
+    setOpenCartSheet(false); // Close the sheet
+    navigate('/shop/checkout'); // Navigate to PaymentPage
+  };
 
   return (
     <SheetContent className="w-full sm:max-w-md">
@@ -27,12 +31,13 @@ function UserCartWrapper({ cartItems, setOpenCartSheet }) {
 
       {/* Scrollable container for cart items */}
       <div className="mt-8 max-h-[50vh] sm:max-h-[60vh] overflow-y-auto space-y-4 px-4">
-        {cartItems && cartItems.length > 0
-          ? cartItems.map((item) => (
+        {cartItems && cartItems.length > 0 ? (
+          cartItems.map((item) => (
             <UserCartItemsContent key={item.id} cartItem={item} />
           ))
-          : <div className="text-center text-gray-400">No items in cart</div>
-        }
+        ) : (
+          <div className="text-center text-gray-400">No items in cart</div>
+        )}
       </div>
 
       <div className="mt-8 space-y-4 px-4">
@@ -43,15 +48,13 @@ function UserCartWrapper({ cartItems, setOpenCartSheet }) {
       </div>
 
       <button
-        onClick={() => navigate('/shop/checkout')} // Navigate to PaymentPage
+        onClick={handleCheckout}
         className="w-full py-3 bg-indigo-900 text-white font-semibold rounded-lg hover:bg-indigo-700 disabled:opacity-50 disabled:cursor-not-allowed"
-        disabled={cartItems.length === 0} // Disable if cart is em
+        disabled={cartItems.length === 0}
       >
-        {cartItems.length === 0 ? 'Cart is Empty' : 'Proceed to Checkout'}
+        {cartItems.length === 0 ? "Cart is Empty" : "Proceed to Checkout"}
       </button>
-
     </SheetContent>
-
   );
 }
 
